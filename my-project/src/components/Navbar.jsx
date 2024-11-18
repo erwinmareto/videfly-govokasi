4fe7f05b1d4efb67ec60b2b44e8a0e7b743b3a84
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-11-18 21:41:24 +0700

    feat: made notification menu responsive

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white shadow-lg rounded-b-lg mb-1">
-      <div className="w-full lg:max-w-7xl lg:mx-auto lg:px-4 lg:sm:px-6">
+      <div className="w-full lg:max-w-7xl lg:mx-auto sm:px-6 lg:px-4 ">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0 ml-4">
@@ -46,8 +46,8 @@ export default function Navbar() {
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />
               </svg>
-              <span className="absolute top-1 right-1 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                5
+              <span className="absolute top-1 right-1 inline-flex items-center justify-center px-2 py-1.5 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-alert rounded-full">
+                3
               </span>
             </button>
 
